8c66a605f1101cc24c9318595f91aecddcd93e31
Jordan Benner <jordanbenner@outlook.com>
2017-06-17 14:34:40 -0500

    fixes#6

--- source/main.js
+++ source/main.js
@@ -84,3 +84,10 @@ frmvalidator.addValidation("name","req","Please provide your name");
 frmvalidator.addValidation("email","req","Please provide your email");
 frmvalidator.addValidation("email","email",
   "Please enter a valid email address");
+
+
+  var requestAnimationFrame = window.requestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      window.oRequestAnimationFrame;
